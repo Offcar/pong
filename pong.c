542e53e0b7b1b500a736570cf940631ffde9d5d3
Offcar <andresortizg8@gmail.com>
2024-07-22 00:29:47 -0400

    master: rendering paddles

--- pong.c
+++ pong.c
@@ -1,33 +1,46 @@
 #include "raylib.h"
-#include "raymath.h"
 #include "stdlib.h"
 #include "stdio.h"
-
-
+ 
 int main (int argc, char** arvg) {
+    int screenWidth = 800;
+    int screenHeigth = 500;
 
-    int screenWidth = 1200;
-    int screenHeigth = 800;
+    int paddleXOffset = 10;
+    int paddleLenght = 100;
+    int paddleWidth = 20;
+
+    int stageYLimit = 30;
+
+    int centerScreenWidth = 0;
+
+    int i = 0;
 
     InitWindow(screenWidth, screenHeigth, "Pong - Raylib/C implementation");
 
     // Loading a png of the title screen
-    Image titleScreen = LoadImage("./resources/titleScreen.png");
-    Texture2D texture = LoadTextureFromImage(titleScreen);
+    /* Image titleScreen = LoadImage("./resources/titleScreen.png"); */
+    /* Texture2D texture = LoadTextureFromImage(titleScreen); */
+    /* UnloadImage(titleScreen); */
 
-    UnloadImage(titleScreen);
-
-    SetTargetFPS(60);
-
+    SetTargetFPS(30);
     while(!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawTexture(texture, 0, 0, RAYWHITE);
+
+        DrawRectangle(0,0,screenWidth,stageYLimit,RAYWHITE);
+        ClearBackground(BLACK);
+        /* DrawRectangle(0,screenHeigth - (), screenWidth,stageYLimit,RAYWHITE); */
+
+        // Paddles
+        DrawRectangle(i + paddleXOffset,0,paddleWidth,paddleLenght,RAYWHITE);
+        DrawRectangle(screenWidth - (paddleWidth + paddleXOffset) ,0,paddleWidth,paddleLenght,RAYWHITE);
 
         EndDrawing();
+
+        i += 10;
     }
 
-    UnloadTexture(texture);
+    /* UnloadTexture(texture); */
     CloseWindow();
 }
 
