d18a50f248700791830d5c351cdbd8f3260ac734
Offcar <andresortizg8@gmail.com>
2024-08-01 01:08:17 -0400

    master: Ball collision

--- pong.c
+++ pong.c
@@ -17,6 +17,7 @@ typedef struct {
     int width;
     int speedX;
     int speedY;
+    int bounces;
 } Ball;
 
 /* typedef enum { } GAMESTATUS; */
@@ -32,8 +33,74 @@ typedef enum {
 #define TOP 30
 #define BOTTOM SHEIGHT - TOP
 #define BALLSPEED 20
+#define MAXBOUNCES 25
 
-void calculatePlayer(Paddle *p, KeyboardKey up, KeyboardKey down) {
+// BALL FUNCTIONS
+void ballReset(Ball *ball, int *bounces) {
+    ball->x = SWIDTH / 2 - (int)ball->width / 2;
+    ball->y = SHEIGHT / 2 - (int)ball->height / 2;
+    ball->speedX = 0;  // BALLSPEED;
+    ball->speedY = BALLSPEED;
+    bounces = 0;
+}
+
+void ballLaunch(Ball *ball) {
+    int randX = GetRandomValue(0, 1);
+    int randY = GetRandomValue(0, 1);
+
+    if (randX == 1) {
+        ball->speedX = BALLSPEED;
+    } else {
+        ball->speedX = -BALLSPEED;
+    }
+
+    if (randY == 1) {
+        ball->speedY = BALLSPEED;
+    } else {
+        ball->speedY = -BALLSPEED;
+    }
+}
+
+void ballCollisionCheck(Ball *b, Paddle *p1, Paddle *p2) {
+    /* int nextX = b->x + b->speedX; */
+    /* int nextX2 = b->x + b->speedX + b->width; */
+    int nextY = b->y + b->speedY;
+    int nextY2 = b->y + b->speedY + b->height;
+
+    if (b->speedY < 0 && nextY < TOP) {  // UP
+        b->speedY *= -1;
+    } else if (b->speedY > 0 && nextY2 > BOTTOM) {  // DOWN
+        b->speedY *= -1;
+    }
+
+    // DEBUGGIN: REBOTE EN EJES X
+    if(b->x < 0 || b->x + b->width > SWIDTH) {
+        b->speedX *= -1;
+    }
+
+    if(b->speedX > 0) {
+        // Check de player 2
+        
+    } else {
+        // Check de player 1
+        if(
+            p1->x < b->x + b->width
+            && p1->x + p1->width > b->x
+            && p1->y < b->y + b->height
+            && p1->y + p1->height >= b->y
+        ) {
+            b->speedX *= -1;
+        }
+    }
+}
+
+void ballUpdatePos(Ball *b) {
+    b->x += b->speedX;
+    b->y += b->speedY;
+}
+
+// PADDLE FUNCTIONS
+void paddlePlayerUpdate(Paddle *p, KeyboardKey up, KeyboardKey down) {
     if (IsKeyDown(up)) {
         if (p->y - p->speed <= TOP) {
             p->y = TOP;
@@ -53,32 +120,6 @@ void calculatePlayer(Paddle *p, KeyboardKey up, KeyboardKey down) {
 
 void calculateCPU(Paddle *CPU) {}
 
-void ballLaunch(Ball *ball) {
-    int randX = GetRandomValue(0, 1);
-    int randY = GetRandomValue(0, 1);
-
-    if (randX == 1) {
-        /* ball->speedX = BALLSPEED; */
-    } else {
-        /* ball->speedX = -BALLSPEED; */
-    }
-
-    if (randY == 1) {
-        ball->speedY = BALLSPEED;
-    } else {
-        ball->speedY = -BALLSPEED;
-    }
-}
-
-void ballReset(Ball *ball, int *bounces) {
-    ball->x = SWIDTH / 2 - (int)ball->width / 2;
-    ball->y = SHEIGHT / 2 - (int)ball->height / 2;
-    ball->speedX = 0; //BALLSPEED;
-    ball->speedY = BALLSPEED;
-
-    bounces = 0;
-}
-
 void paddleReset(Paddle *p1, Paddle *p2) {
     p1->x = 30;
     p1->y = SHEIGHT / 2 - 50;
@@ -87,45 +128,27 @@ void paddleReset(Paddle *p1, Paddle *p2) {
     p2->y = p1->y;
 }
 
-void calculatePaddles(Paddle *p1, Paddle *p2, int isPlayer2Enabled) {
-    calculatePlayer(p1, KEY_W, KEY_S);
+void paddlesUpdate(Paddle *p1, Paddle *p2, int isPlayer2Enabled) {
+    paddlePlayerUpdate(p1, KEY_W, KEY_S);
 
     if (isPlayer2Enabled == 1) {
-        calculatePlayer(p2, KEY_UP, KEY_DOWN);
+        paddlePlayerUpdate(p2, KEY_UP, KEY_DOWN);
     } else {
         calculateCPU(p2);
     }
 }
 
-void DrawPaddles(Paddle *p1, Paddle *p2, Color color) {
+// Draw functions
+void DrawPlayableElements(Paddle *p1, Paddle *p2, Ball *b, Color color) {
     DrawRectangle(p1->x, p1->y, p1->width, p1->height, color);
     DrawRectangle(p2->x, p2->y, p2->width, p2->height, color);
+    DrawRectangle(b->x, b->y, b->width, b->height, color);
 }
 
 void DrawBackground() {
     DrawRectangle(0, 0, SWIDTH, TOP, GRAY);
     DrawRectangle(0, BOTTOM, SWIDTH, BOTTOM, GRAY);
     DrawLine(SWIDTH / 2, 0, SWIDTH / 2, SHEIGHT, GRAY);
-}
-
-void DrawBall(Ball *b, Color color) {
-    DrawRectangle(b->x, b->y, b->width, b->height, color);
-}
-
-void calculateBallNextStep(Ball *b) {
-    int nextStepX = b->x + b->speedX;
-    int nextStepY = b->y + b->speedY;
-
-    if (b->speedY < 0 && nextStepY < TOP) {  // UP
-        b->speedY *= -1;
-    } else if (b->speedY > 0 && nextStepY + b->height > BOTTOM) {  // DOWN
-        b->speedY *= -1;
-    }
-
-
-
-    b->x += b->speedX;
-    b->y += b->speedY;
 }
 
 int main(int argc, char **arvg) {
@@ -142,21 +165,21 @@ int main(int argc, char **arvg) {
 
     SetTargetFPS(30);
     while (!WindowShouldClose()) {
+        /*
         printf(
             "Step: %d | ballX: %d | ballY: %d | ballSpeedX: %d | ballSpeedY: "
             "%d \n",
             i, ball.x, ball.y, ball.speedX, ball.speedY);
+        */
 
         switch (round) {
             case WAIT:
                 BeginDrawing();
                 ClearBackground(BLACK);
 
-                DrawPaddles(&p1, &p2, GRAY);
-                DrawBall(&ball, GRAY);
                 DrawBackground();
+                DrawPlayableElements(&p1, &p2, &ball, GRAY);
 
-                // Centrar bien yisus christ
                 DrawText("PRESS SPACE TO START ROUND", SWIDTH / 2 - 180,
                          SHEIGHT / 2 - 20, 20, RAYWHITE);
 
@@ -171,16 +194,15 @@ int main(int argc, char **arvg) {
                     ballLaunch(&ball);
                 }
 
-                calculatePaddles(&p1, &p2, 1);
-
-                calculateBallNextStep(&ball);
-
+                paddlesUpdate(&p1, &p2, 1);
+                ballCollisionCheck(&ball, &p1, &p2);
+                ballUpdatePos(&ball);
+                
                 BeginDrawing();
                 ClearBackground(BLACK);
 
                 DrawBackground();
-                DrawPaddles(&p1, &p2, RAYWHITE);
-                DrawBall(&ball, RAYWHITE);
+                DrawPlayableElements(&p1, &p2, &ball, RAYWHITE);
                 EndDrawing();
 
                 // ball increases speed on every bounce ??
